95a75427247b4957881fc3b7024063af32f615a0
ahmettaydinn <birceste@gmail.com>
2023-04-07 14:16:47 +0300

    backend-3 add create article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ActionController::Base
 
     def show    
-
         @article = Article.find(params[:id])
+     end
 
+    def index    
+        @articles = Article.all
      end
      
 
+    def new    
+     
+     end
+     
+
+    def create    
+     render plain: params[:article]
+     end
+     
+ 
 end
